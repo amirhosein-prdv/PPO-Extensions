80ecd1ddf1fbc195f916b3ac90e629bf594e5d9a
amirhosein <ah.pourdavoud79@gmail.com>
2026-05-21 18:56:26 +0330

    Moved the environment functions to another file

--- MetaPPO_main.py
+++ MetaPPO_main.py
@@ -1,61 +1,8 @@
-import random
-import numpy as np
-import metaworld
-import gymnasium as gym
-
+from MetaPPO.MakeEnv import *
 from MetaPPO.MAML_PPO import MAMLPPO as Meta
 
 # from MetaPPO.MAML_TRPO import MAMLTRPO as Meta
 # from MetaPPO.Reptile_PPO import ReptilePPO as Meta
-
-
-def create_pendulum_env():
-    """Create Pendulum environment with varying parameters."""
-    env = gym.make("Pendulum-v1")
-
-    # Modify environment parameters for task variation
-    env.unwrapped.g = np.random.uniform(5, 15)
-
-    # env.unwrapped.max_speed = np.random.uniform(6, 10)
-    # env.unwrapped.max_torque = np.random.uniform(1.5, 2.5)
-
-    return env
-
-
-def create_meta_world_env():
-    """
-    Samples from MetaWorld ML10 TEST tasks (no leakage).
-    """
-    ml10 = metaworld.ML10()
-
-    env_name = random.choice(list(ml10.test_classes.keys()))
-    env = ml10.test_classes[env_name]()
-
-    task = random.choice([t for t in ml10.test_tasks if t.env_name == env_name])
-    env.set_task(task)
-
-    return env
-
-
-def create_reach_env(eval: bool = False):
-    """
-    Create MetaWorld ML1 reach-v3 environment.
-    ML1 = single task (reach), different goals.
-    """
-
-    ml1 = metaworld.ML1("reach-v3")
-
-    if not eval:
-        env = ml1.train_classes["reach-v3"]()
-        tasks = ml1.train_tasks
-    else:
-        env = ml1.test_classes["reach-v3"]()
-        tasks = ml1.test_tasks
-
-    task = random.choice(tasks)
-    env.set_task(task)
-
-    return env
 
 
 def env_factory():
